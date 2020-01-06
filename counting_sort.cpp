8f03ddd6784316e106433ee4b3d6b88a23050838
SergioAlmeidaCiprianoJr <36544528+SergioAlmeidaCiprianoJr@users.noreply.github.com>
2020-01-06 19:29:26 -0300

    Update counting_sort.cpp

--- counting_sort.cpp
+++ counting_sort.cpp
@@ -21,16 +21,16 @@ void countSort(vector <int>& arr)
 
 	vector<int> count(range), output(arr.size()); 
 	for(int i = 0; i < arr.size(); i++) 
-	  count[arr[i]-min]++; 
+		count[arr[i]-min]++; 
 	    
 	for(int i = 1; i < count.size(); i++) 
-	     count[i] += count[i-1]; 
+		count[i] += count[i-1]; 
 
 	for(int i = arr.size()-1; i >= 0; i--)
-			output[ --count[arr[i]-min] ] = arr[i];
+		output[ --count[arr[i]-min] ] = arr[i];
 
 	for(int i=0; i < arr.size(); i++) 
-			arr[i] = output[i]; 
+		arr[i] = output[i]; 
 } 
 void printArray(vector <int> & arr)
 { 
